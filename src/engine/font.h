0980f7402834ee51cda775d01cf8ce8d069a31b5
Juan Pablo Ossa <ozjuanpa@gmail.com>
2020-03-04 17:21:55 -0500

    Implement font loading in engine

---
+++ src/engine/font.h
@@ -0,0 +1,37 @@
+#ifndef ENGINE_FONT_H
+#define ENGINE_FONT_H
+
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+typedef struct {
+  unsigned char data[8];
+} CharFont;
+
+typedef struct {
+  CharFont characters[128];
+} Font;
+
+Font *loadFont (char *path) {
+
+  // Get file descriptor.
+  FILE *fd = fopen(path, "r");
+  if (fd == NULL) {
+    return NULL;
+  }
+
+  // Load font data from file.
+  Font *self = calloc(1, sizeof(Font));
+  for (unsigned int charid = 0; charid < 128; charid++) {
+    for (unsigned int row = 0; row < 8; row++) {
+      self->characters[charid].data[row] = fgetc(fd);
+    }
+  }
+
+  fclose(fd);
+  return self;
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,23 +1,27 @@
 #include "engine/engine.h"
 #include "engine/drawing.h"
+#include "engine/font.h"
 #include <stdio.h>
 
 Engine *engine;
 Texture *sample;
+Font *testfont;
 
 
 void draw_test () {
-  draw_clear(engine, 0x11);
+  draw_clear(engine, 0b10000000);
   draw_texture_options(engine, sample,
       10, 15, 0,
       0, 64, 64,
       0, 0);
-  
+
 }
 
 int main(int argc, const char *argv[]) {
 
   sample = loadTexture("assets/graphics/sample.hg");
+  testfont = loadFont("assets/fonts/default.hf");
+  printf("%u\n", testfont->characters[0].data[1]);
   printf("%u\n", sample->side);
   engine = newEngine(256, 200, 3, "mkpixel");
   engine->on_draw = draw_test;
